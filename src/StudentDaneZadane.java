5674c7c63ee5e998a179e3310559eeea16b55803
s.wojcik74 <s.wojcik74@wp.pl>
2023-10-28 12:15:44 +0200

    First Modification

---
+++ src/StudentDaneZadane.java
@@ -0,0 +1,10 @@
+public class StudentDaneZadane
+{
+    public static void main(String[] args)
+    {
+        // program wyświetli na ekranie z góry zadane dane studenta
+        System.out.println("Adam");
+        System.out.println("Slodowy");
+        System.out.println("Nowy Sacz");
+    }
+}
